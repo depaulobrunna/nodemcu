fb9439ff441e1122d09e1b076bde412090bfba85
Brunna <depaulobr@gmail.com>
2018-10-04 17:29:53 -0300

    tentativa de instalar o pyb module, porem sem internet

--- main.py
+++ main.py
@@ -2,23 +2,14 @@ import esp
 import machine
 #from machine import Pin, PWM
 import time
+import pyb
 
 def main():
-    servo = machine.PWM(machine.Pin(5), freq=50)
-    print('something')
-    '''p5 = machine.Pin(5)
-    pwm5 = machine.PWM(p5)
-    pwm5.freq(50)
-    pwm5.duty(25)
+    #pyb.delay(20)
     while(True):
-        #servo.duty(44)'''
-    while(True):
-        servo.duty(58)
-        time.sleep_ms(3000)
-        servo.duty(100)
-        time.sleep_ms(3000)
-        #servo.duty(77)
-        #time.sleep_ms(500)
+        print('something')
+        pyb.delay(1000)
+        
 
 if __name__ == '__main__':
     print("main entered")
