1cfcbe2dada0c2c286e6771235dd55756aeca521
Brunna <depaulobr@gmail.com>
2018-10-04 17:04:43 -0300

    range do servo feita

--- boot.py
+++ boot.py
@@ -1,7 +1,8 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
+import esp
+esp.osdebug(0)
 import gc
 #import webrepl
 #webrepl.start()
 gc.collect()
+
--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import esp
-from machine import Pin
+import machine
+#from machine import Pin, PWM
 import time
 
 def main():
-    p5 = Pin(5, Pin.IN)    
+    servo = machine.PWM(machine.Pin(5), freq=50)
+    print('something')
+    '''p5 = machine.Pin(5)
+    pwm5 = machine.PWM(p5)
+    pwm5.freq(50)
+    pwm5.duty(25)
     while(True):
-        print(p5.value())
-        
+        #servo.duty(44)'''
+    while(True):
+        servo.duty(58)
+        time.sleep_ms(3000)
+        servo.duty(100)
+        time.sleep_ms(3000)
+        #servo.duty(77)
+        #time.sleep_ms(500)
+
 if __name__ == '__main__':
     print("main entered")
-    esp.osdebug(0)
     main()
