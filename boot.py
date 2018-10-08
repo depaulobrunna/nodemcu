7ef61d32661be68dc8c36a49636e7bef690ccd24
Brunna <depaulobr@gmail.com>
2018-10-08 09:23:13 -0300

    tentativas de instalar pyb module mas este está indisponivel para o esp8226

--- boot.py
+++ boot.py
@@ -5,4 +5,15 @@ import gc
 #import webrepl
 #webrepl.start()
 gc.collect()
-
+print('boot')
+'''def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('LIT', 'l17@2017')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+do_collect()'''
--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import esp
 import machine
 #from machine import Pin, PWM
 import time
-import pyb
-
+#import pyb
+    
 def main():
     #pyb.delay(20)
     while(True):
-        print('something')
-        pyb.delay(1000)
+        print('main')
+        time.sleep(3000)
+        #pyb.delay(1000)
         
-
-if __name__ == '__main__':
+main()
+'''if __name__ == '__main__':
     print("main entered")
-    main()
+    main()'''
